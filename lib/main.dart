81c82cb3388d655149302091c8bf741eaf7c2352
AidAppDev <161708220+AidAppDev@users.noreply.github.com>
2025-05-01 17:52:03 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,36 @@
+import 'package:helpables/Providers/aidrequests_provider.dart';
+
+import '../Modal/LocationHelper.dart';
+import '../Providers/User_provider.dart';
+import '../Providers/add_aid_requestprov.dart';
+import '../Providers/categories_providers.dart';
+import '../Providers/login_and_signup_provider.dart';
+import '../Screens/login.dart';
+import '../Screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  const String apiKey =
+      String.fromEnvironment("GOOGLE_MAPS_API_KEY", defaultValue: "");
+  await LocationHelper.saveApiKey(apiKey);
+  await Firebase.initializeApp();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => LoginAndSignupProvider()),
+        ChangeNotifierProvider(create: (context) => AddAidRequestProvider()),
+        ChangeNotifierProvider(create: (context) => UserInfoProvider()),
+        ChangeNotifierProvider(create: (context) => CategoriesProvider()),
+        ChangeNotifierProvider(create: (context) => AidRequestsProvider()),
+      ],
+      child: MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
